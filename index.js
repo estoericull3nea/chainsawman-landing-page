62917e729973d64411226d0630cfa408cb8e8c05
Ericson Palisoc <palisocericson87@gmail.com>
2023-06-27 23:07:32 +0800

    sound issue fixed

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-window.addEventListener('DOMContentLoaded', function () {
-  var audio = document.getElementById('test')
-  audio.play()
-})
+const vol = document.querySelector('.fa-volume-xmark')
+const myAudio = document.querySelector('.myAudio')
+
+if (vol) {
+  vol.addEventListener('click', () => {
+    if (myAudio.paused) {
+      myAudio.play()
+      vol.classList.remove('fa-volume-xmark')
+      vol.classList.add('fa-volume-high')
+    } else {
+      myAudio.pause()
+      vol.classList.add('fa-volume-xmark')
+      vol.classList.remove('fa-volume-high')
+    }
+  })
+}
